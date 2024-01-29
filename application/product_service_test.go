c2b44ca6f104432874b46362e7d5763dbb0a62e2
Túlio Fergulha <Tulio.Fergulha@Ab-Inbev.com>
2024-01-29 19:52:49 -0300

    Implemented Get product method into service

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,1 +1,28 @@
 package application_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"github.com/tuliofergulha/arch-hexagonal/application"
+	mock_application "github.com/tuliofergulha/arch-hexagonal/application/mocks"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
