62c1995194110b043d4fb763d30c94fa367547fe
Túlio Fergulha <Tulio.Fergulha@Ab-Inbev.com>
2024-01-30 16:15:58 -0300

    Create an http handler

---
+++ adapters/web/server/server.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/tuliofergulha/arch-hexagonal/adapters/web/handler"
+	"github.com/tuliofergulha/arch-hexagonal/application"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      http.DefaultServeMux,
+		ErrorLog:     log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -16,7 +16,9 @@ require (
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/codegangsta/negroni v1.0.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.1
 	github.com/mattn/go-sqlite3 v1.14.20
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/satori/go.uuid v1.2.0
